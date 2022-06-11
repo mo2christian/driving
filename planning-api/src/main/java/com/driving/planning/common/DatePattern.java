1f9d787c41352e74d753e794bf45bbf42deaa083
Christian N. MOMO <46202997+mo2christian@users.noreply.github.com>
2022-06-11 17:08:07 +0200

    fix(api): add second optional in time format (#44)
    Co-authored-by: Christian N. MOMO <christian.momo@monlabo.biz>

--- planning-api/src/main/java/com/driving/planning/common/DatePattern.java
+++ planning-api/src/main/java/com/driving/planning/common/DatePattern.java
@@ -4,9 +4,7 @@ public class DatePattern {
 
     public static final String DATE = "yyyy-MM-dd";
 
-    public static final String TIME = "HH:mm";
-
-    public static final String DATE_TIME = "yyyy-MM-dd HH:mm";
+    public static final String TIME = "HH:mm[:ss]";
 
     private DatePattern() {
     }
