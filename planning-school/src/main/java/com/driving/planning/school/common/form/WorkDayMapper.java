814841f7014b013671dc71d5a16e449da40772ea
Christian N. MOMO <christian.momo@monlabo.biz>
2022-06-12 13:25:22 +0200

    fix(api): add second optional in time format

--- planning-school/src/main/java/com/driving/planning/school/common/form/WorkDayMapper.java
+++ planning-school/src/main/java/com/driving/planning/school/common/form/WorkDayMapper.java
@@ -8,13 +8,13 @@ import org.mapstruct.Mapping;
 public interface WorkDayMapper {
 
 
-    @Mapping(target = "begin", source = "hourly.begin",  dateFormat = "HH:mm")
-    @Mapping(target = "end", source = "hourly.end", dateFormat = "HH:mm")
+    @Mapping(target = "begin", source = "hourly.begin",  dateFormat = "HH:mm[:ss]")
+    @Mapping(target = "end", source = "hourly.end", dateFormat = "HH:mm[:ss]")
     @Mapping(target = "selected", constant = "true")
     WorkDayForm hourlyToForm(Hourly hourly);
 
-    @Mapping(target = "begin", dateFormat = "HH:mm")
-    @Mapping(target = "end", dateFormat = "HH:mm")
+    @Mapping(target = "begin", dateFormat = "HH:mm[:ss]")
+    @Mapping(target = "end", dateFormat = "HH:mm[:ss]")
     Hourly formToHourly(WorkDayForm from);
 
 
